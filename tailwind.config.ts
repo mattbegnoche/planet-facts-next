0f5bd86b59fb0d4f42488ebc2cfdc1f4fc665492
Matthew Begnoche <b3gnoche@gmail.com>
2023-11-08 08:59:41 -0600

    Fonts added and colors set in tailwind
    Colors and fonts set.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        white: "#fff",
+        merucry: "#419EBB",
+        venus: "#EDA249",
+        earth: "#6D2ED5",
+        mars: "#D14C32",
+        jupiter: "#D83A34",
+        saturn: "#CD5120",
+        uranus: "#1EC1A2",
+        neptune: "#2D68F0",
+        planetGray: {
+          300: "#838391",
+          700: "#38384F",
+          900: "#070724",
+        },
+      },
+      fontFamily: {
+        mono: ["var(--font-antonio)"],
+        sans: ["var(--font-spartan)"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
